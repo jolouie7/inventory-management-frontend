c2db49f32bbadb7c47594dc38699898d10a01b4e
Your_Name <joseph161216@gmail.com>
2020-08-13 16:15:07 -0700

    basic navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import LandingPage from "./components/LandingPage"
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <LandingPage />
     </div>
   );
